12f2536166c6248568dae5f866edb4769d745e3a
efiwoncharso51017008 <efiwoncharso5@gmail.com>
2019-12-10 15:16:01 +0700

    second commit

---
+++ js/sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = "pwebmovie";
+var urlsToCache = [
+  "/",
+  "/js/tmdb.js",
+  "/js/materialize.min.css",
+  "/js/init.js",
+  "/js/api.js",
+  "/css/materialize.min.css",
+  "/img/movie_1080.jpg",
+  "/img/movie_720.jpg",
+  "/img/movie_480.jpg",
+  "/css/style.css",
+  "/manifest.json",
+];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    cashes
+    .match(event.request, {cacheName: CACHE_NAME})
+    .then(function (response) {
+      if (response) {
+        console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+        return response;
+      }
+      console.log(
+        "ServiceWorker: Memuat aset dari server: ",
+        event.request.url
+      );
+      return fetch(event.request);
+    })
+  );
+});
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + "dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
